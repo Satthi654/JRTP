eb3b0e3e64831bca020daaeb1554878f186a7fc4
Satthi654 <Satthi654@gmail.com>
2023-01-24 15:59:35 +0530

    Three Files commites at a time

---
+++ Employee.java
@@ -0,0 +1,18 @@
+public class Employee{
+	
+	public static void main(String[] args){
+		
+	}
+	
+	boolean button=false;
+	public boolean handleVisibility(){
+		if(button==true)
+		{
+			return button=true;
+		}
+		else
+		{
+			return button=false;
+		}
+	}
+}
