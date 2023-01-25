d2b098fbaa5ed13fe62d958aad53ab4d63579634
Satthi654 <Satthi654@gmail.com>
2023-01-25 15:14:30 +0530

    commited Test1.java and Test2.java

---
+++ Test1.java
@@ -0,0 +1,5 @@
+public class Test1{
+	public static void main(String[] args){
+		System.out.println("we are going to rock in java");
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,8 @@
+public class Test2{
+	public void method1(){
+	
+	}
+	public void method2(){
+	
+	}
+}
