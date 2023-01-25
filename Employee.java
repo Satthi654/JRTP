cc3c2c6740d797aa0e825d5ce6e1262647c7af90
Satthi654 <Satthi654@gmail.com>
2023-01-25 15:29:38 +0530

    Changes Commited to Employee.java

--- Employee.java
+++ Employee.java
@@ -16,6 +16,21 @@ public class Employee{
 		}
 	}
 	public void performSubmit(){
-		
+		if(con==nul)
+		{
+			initDb();
+		}
+		if(con!=null)
+		{
+			try
+			{
+				FacesMessage message=new FacesMessage("SUCCESS","Submitted Successfully");
+				FacesContext.getCurrentInstance().addMessage(null, message);
+			}
+			catch(EXception e)
+			{
+				FacesContext.getCurrentInstance.addMessage(null,new FaceMessage(FacesMessage.SEVERITY_ERROR,"ERROR","Error is"+e.getLocalizedMessage());
+			}
+		}
 	}
 }
