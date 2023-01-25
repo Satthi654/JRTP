31ad57f1024265ea971b5774cb613216d8463855
Satthi654 <Satthi654@gmail.com>
2023-01-25 15:23:17 +0530

    Practice 1 commited

---
+++ Test3.java
@@ -0,0 +1,17 @@
+public class Test3{
+	public static void main(String[] args){
+		
+	}
+	
+	public static void method1(){
+		
+	}
+	
+	public static void method2(){
+		
+	}
+	
+	public static void method3(){
+		
+	}
+}
