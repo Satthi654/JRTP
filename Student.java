056bd14f201f6b3a6741d4c0738e0cfed75e3479
Satthi654 <Satthi654@gmail.com>
2023-01-25 16:02:04 +0530

    commited again

--- Student.java
+++ Student.java
@@ -7,8 +7,34 @@ public class Student{
 	public void saveMethod(){
 		
 	}
+
 	public void saveMethod1(){
 		System.out.pritln("git stash example");
 	}
 	
+	
+	public int subject1,subject2,subject3;
+	public int total,gained,percentage;
+	public void  decideResult(){
+		total=300;
+		gained=subject1+subject2+subject3;
+		percentage=gained/total*100;
+		if(percentage>=85)
+		{
+			System.out.println("Distinction");
+		}
+		else if(percentage>=60)
+		{
+			System.out.println("First Class");
+		}
+		esle if(percentage==35)
+		{
+			System.out.println("Pass");
+		}
+		else
+		{
+			System.out.println("Failed");
+		}
+	}
+
 }
